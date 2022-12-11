625d66af1e6bc70825480cfb9b938f9fb1358e16
Biser Stoilov <lvim@motion-agency.com>
2022-12-11 16:41:48 +0200

    Fix colors

--- lua/lvim-colorscheme/extra/xresources.lua
+++ lua/lvim-colorscheme/extra/xresources.lua
@@ -7,26 +7,26 @@ function M.generate(colors)
         [[
 ! Lvim Xresources Colors
 
-*background: ${color_03}
-*foreground: ${color_21}
+*background: ${bg_01}
+*foreground: ${fg_05}
 
-*color0: ${color_03}
-*color1: ${color_25}
-*color2: ${color_18}
-*color3: ${color_07}
-*color4: ${color_13}
-*color5: ${color_24}
-*color6: ${color_23}
-*color7: ${color_16}
+*color0: ${bg_01}
+*color1: ${red_02}
+*color2: ${green_02}
+*color3: ${orange_02}
+*color4: ${blue_02}
+*color5: ${teal_02}
+*color6: ${cyan_01}
+*color7: ${fg_05}
 
-*color8: ${color_23}
-*color9: ${color_25}
-*color10: ${color_18}
-*color11: ${color_07}
-*color12: ${color_13}
-*color13: ${color_24}
-*color14: ${color_23}
-*color15: ${color_21}
+*color8: ${orange_03}
+*color9: ${red_03}
+*color10: ${green_03}
+*color11: ${orange_03}
+*color12: ${blue_03}
+*color13: ${teal_03}
+*color14: ${cyan_02}
+*color15: ${fg_06}
 ]],
         colors
     )
