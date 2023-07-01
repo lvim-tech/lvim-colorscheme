b384a7f125d73bfd73c8a923f6b7ee80a784ee37
LVIM ORG <lvim@motion-agency.com>
2023-07-01 21:14:44 +0300

    Everforest visual

--- lua/lvim-colorscheme/colors/lvim-gruvbox-dark-soft.lua
+++ lua/lvim-colorscheme/colors/lvim-gruvbox-dark-soft.lua
@@ -62,7 +62,7 @@ colors.bg_statusline = utils.darken(colors.bg_01, 0.7, "#141414")
 colors.bg_popup = utils.darken(colors.bg_01, 0.7, "#141414")
 colors.bg_float = utils.darken(colors.bg_01, 0.7, "#141414")
 colors.bg_visual = utils.darken(colors.bg_04, 0.6, "#141414")
-colors.bg_search = utils.darken(colors.bg_05, 0.6, "#141414")
+colors.bg_search = utils.darken(colors.bg_05, 0.5, "#141414")
 colors.bg_cursor_line = utils.darken(colors.bg_04, 0.6, "#141414")
 colors.fg_sidebar = colors.orange_02
 
