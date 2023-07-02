5f5cc23e07e539b0c9a7c8ee0cd286fec05ef4d3
LVIM ORG <lvim@motion-agency.com>
2023-07-02 15:28:47 +0300

    Add catppuccin theme

---
+++ lua/lvim-colorscheme/colors/lvim-catppuccin-dark-soft.lua
@@ -0,0 +1,69 @@
+local utils = require("lvim-colorscheme.utils")
+local colors = {
+    NONE = "NONE",
+    white = "#FFFFFF",
+    bg = "#1F222E", -- +2
+    bg_01 = "#232634", -- base
+    bg_02 = "#252837", -- -1
+    bg_03 = "#272A3A", -- -1
+    bg_04 = "#40445F", -- -12
+    bg_05 = "#4A4F6E", -- -5
+    bg_06 = "#555A7D", -- -5
+    bg_07 = "#5F658D", -- -5
+    fg_01 = "#B5BFE2",
+    fg_02 = "#A5ADCE",
+    fg_03 = "#949CBB",
+    fg_04 = "#838BA7",
+    fg_05 = "#737994",
+    fg_06 = "#626880",
+    fg_07 = "#448589",
+    blue_01 = "#0C97D3",
+    blue_02 = "#0B8EC6",
+    blue_03 = "#0A85B9",
+    teal_01 = "#02A384",
+    teal_02 = "#02967A",
+    teal_03 = "#028970",
+    cyan_01 = "#019AB3",
+    cyan_02 = "#018FA6",
+    cyan_03 = "#018499",
+    green_01 = "#95B266",
+    green_02 = "#8AA55F",
+    green_03 = "#7F9858",
+    red_01 = "#E34A39",
+    red_02 = "#D64636",
+    red_03 = "#C94233",
+    orange_01 = "#FF9C2A",
+    orange_02 = "#F29428",
+    orange_03 = "#E58C26",
+    git = {
+        add = "#93AC6C",
+        change = "#FF9A3C",
+        delete = "#DD5647",
+        conflict = "#FF9A3C",
+        ignore = "#0098AD",
+    },
+    gitSigns = {
+        add = "#93AC6C",
+        change = "#FF9A3C",
+        change_delete = "#DC7214",
+        delete = "#DD5647",
+        top_delete = "#B52E1F",
+        untracked = "#0A85B9",
+    },
+    diff = { add = "#323926", change = "#48341B", delete = "#481C17", text = "#242B30" },
+}
+colors.success = colors.green_02
+colors.error = colors.red_02
+colors.warning = colors.orange_02
+colors.info = colors.blue_02
+colors.hint = colors.teal_02
+colors.bg_sidebar = utils.darken(colors.bg_01, 0.7, "#141414")
+colors.bg_statusline = utils.darken(colors.bg_01, 0.7, "#141414")
+colors.bg_popup = utils.darken(colors.bg_01, 0.7, "#141414")
+colors.bg_float = utils.darken(colors.bg_01, 0.7, "#141414")
+colors.bg_visual = utils.darken(colors.bg_04, 0.6, "#141414")
+colors.bg_search = utils.darken(colors.bg_05, 0.6, "#141414")
+colors.bg_cursor_line = utils.darken(colors.bg_04, 0.6, "#141414")
+colors.fg_sidebar = colors.orange_02
+
+return colors
