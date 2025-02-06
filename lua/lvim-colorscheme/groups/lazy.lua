3b864c334832ac6a93e978b5e45be1996bff549a
LVIM ORG <lvim@motion-agency.com>
2025-02-06 09:49:18 +0200

    Lazy.nvim colors

--- lua/lvim-colorscheme/groups/lazy.lua
+++ lua/lvim-colorscheme/groups/lazy.lua
@@ -6,9 +6,11 @@ M.url = "https://github.com/folke/lazy.nvim"
 function M.get(c)
   -- stylua: ignore
   return {
-    LazyNormal = { bg = c.bg_float, fg = c.green_dark },
-    LazyProgressDone = { bold = true, fg = c.magenta_dark },
-    LazyProgressTodo = { bold = true, fg = c.fg_dark },
+    LazyNormal        = { bg = c.bg_float, fg = c.green_dark },
+    LazyDimmer        = { fg = c.fg_soft_dark },
+    LazyProp          = { fg = c.yellow_dark },
+    LazyProgressDone  = { bold = true, fg = c.magenta_dark },
+    LazyProgressTodo  = { bold = true, fg = c.fg_dark },
   }
 end
 
