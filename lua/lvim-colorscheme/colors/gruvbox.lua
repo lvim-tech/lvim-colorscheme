bcccaed94208ae73f6cdd2902fb74930fcff46fc
LVIM ORG <lvim@motion-agency.com>
2025-04-09 17:01:03 +0300

    Add Gruvbox

---
+++ lua/lvim-colorscheme/colors/gruvbox.lua
@@ -0,0 +1,46 @@
+return {
+
+    bg_light = "#353231",
+    bg_soft_light = "#2c2a29",
+    bg = "#222120",
+    bg_soft_dark = "#222120",
+    bg_dark = "#1d1c19",
+
+    bg_highlight = "#433f3e",
+
+    fg_light = "#746c63",
+    fg_soft_light = "#6c645c",
+    fg = "#645c54",
+    fg_soft_dark = "#5c544d",
+    fg_dark = "#544c45",
+
+    comment = "#63584f",
+
+    blue = "#42728b",
+    blue_dark = "#3a6479",
+    green = "#75783a",
+    green_dark = "#656831",
+    cyan = "#5d7e69",
+    cyan_dark = "#526f5d",
+    magenta = "#c4746e",
+    magenta_dark = "#c16d67",
+    orange = "#cc7942",
+    orange_dark = "#b86d3c",
+    yellow = "#af9e6b",
+    yellow_dark = "#a6935a",
+    purple = "#635d71",
+    purple_dark = "#575163",
+    red = "#ce5f57",
+    red_dark = "#bb544d",
+    teal = "#759c73",
+    teal_dark = "#678b66",
+
+    terminal_bg = "#63584f",
+    git = {
+        add = "#5f7240",
+        change = "#bf954a",
+        delete = "#ce5f57",
+        change_delete = "#cc7942",
+        untracked = "#759c73",
+    },
+}
