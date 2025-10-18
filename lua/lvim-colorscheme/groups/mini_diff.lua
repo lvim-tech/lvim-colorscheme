8cad48cd0957b3532ce6fef67d3855f9532937b1
lvim-tech <biserstoilov@gmail.com>
2025-10-18 10:29:46 +0300

    Mini.diff colors

--- lua/lvim-colorscheme/groups/mini_diff.lua
+++ lua/lvim-colorscheme/groups/mini_diff.lua
@@ -4,11 +4,15 @@ M.url = "https://github.com/echasnovski/mini.files"
 
 ---@type lvim-colorscheme.HighlightsFn
 function M.get(c)
+    local blend = c.blend
     -- stylua: ignore
     return {
         MiniDiffSignAdd= { fg = c.git.add },
         MiniDiffSignChange= { fg = c.git.change },
         MiniDiffSignDelete= { fg = c.git.delete },
+        MiniDiffOverChangeBuf = { bg = blend.blueLow },
+        MiniDiffOverContext = { bg = blend.blueHigh },
+        MiniDiffOverContextBuf = { bg = c.black }
     }
 end
 
